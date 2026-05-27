fd2ebd11238d814a22158edfe3bc2828304cb206
MasoHome <mirsrbinovski@gmail.com>
2026-05-27 19:54:30 +1000

    test changes

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // 1. Create a client
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  // <React.StrictMode>
     {/* 2. Provide the client to your App */}
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 );
