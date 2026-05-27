90f84e9c8445f358c3b2d12145b4992345c31843
MasoHome <mirsrbinovski@gmail.com>
2026-05-27 19:56:56 +1000

    get back React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     {/* 2. Provide the client to your App */}
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  // </React.StrictMode>,
+  </React.StrictMode>,
 );
