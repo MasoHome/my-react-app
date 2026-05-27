66dbc7a8cfa8b020b8bf8f512c9041f13cbba6bf
MasoHome <mirsrbinovski@gmail.com>
2026-05-27 20:40:33 +1000

    Chnage to the pages

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Welcome() {
   return (
     <div>
       <h1>Welcome to my App!</h1>
+      <p>This is the home page.</p>
+      
+      {/* Adding the link here */}
+      <Link to="/users">Go to User List</Link>
     </div>
   );
 }
