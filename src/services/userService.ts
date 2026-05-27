a9e05d601c78895e3eed500d80f9b5410b0b46a6
MasoHome <mirsrbinovski@gmail.com>
2026-05-27 18:45:52 +1000

    Small change to have error checking in the user service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -5,8 +5,9 @@ export const useUsers = () => {
   return useQuery({
     queryKey: ['users'],
     queryFn: async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/users');
-      return res.json();
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!response.ok) throw new Error('Network response was not ok');
+      return response.json();
     }
   });
 };
