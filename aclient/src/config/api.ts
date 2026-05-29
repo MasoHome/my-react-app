f809941da27c958574b1c39492950a8f8bdcc1f5
MasoHome <mirsrbinovski@gmail.com>
2026-05-29 14:54:26 +1000

    change from localhost to DEV_API_URL

--- aclient/src/config/api.ts
+++ aclient/src/config/api.ts
@@ -1,5 +1,5 @@
 // client/src/config/api.ts
-export const API_BASE = 'http://localhost:3000/api';
+export const API_BASE = 'http:DEV_API_URL:3000/api';
 export const ENDPOINTS = {
   USERS: `${API_BASE}/users`,
   PRODUCTS: `${API_BASE}/products`,
